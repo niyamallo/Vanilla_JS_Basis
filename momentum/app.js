dd64a03f7bad069afed2897a4c064b6be2bba601
niyamallo <treenicole@naver.com>
2023-07-07 02:01:26 +0900

    20230707

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,57 @@
 // alert("hi");
+
+// const는 상수. 값이 바뀔 수 없음.
+// const a = 5;
+// const b = 2;
+
+// const myName = "niyamallo";
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName); 
+
+// const a = 5;
+// const b = 2;
+
+// // let은 업데이트 될 수 있는 변수에 사용.
+// // 기본적으로는 const를 사용하고 let을 필요한 곳에 사용
+// // var는 사용하지 말자
+// let myName = "niyamallo";
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName); 
+
+// // let은 처음 선언할 때 사용하면 됨.
+// myName = "niya";
+
+// // let 대신에 const로 선언했었다면,
+// // app.js:25 Uncaught TypeError: Assignment to constant variable. 에러발생
+// console.log("your new name is " + myName);
+
+// boolean (true or false)
+// null은 아무것도 없다는 것을 뜻함. ('아무것도 없음'으로 채운 것.)
+// undefined는 존재하지 않는, 어떠한 데이터 타입도 아닌 것
+// const amIFat = null;
+// // 선언만 하고 값을 주지 않을 수 있음. 그러면 출력시 undefined
+// let something;
+// console.log(something, amIFat); // undefined null
+// 기억할 것! null은 절대 자연적으로 발생하지 않는다.
+// variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것.
+
+// array
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// console.log(daysOfWeek); // ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+// // Get Item from Array
+// console.log(daysOfWeek[4]); // fri
+
+// // Add one more day to the array
+// daysOfWeek.push("sun");
+// console.log(daysOfWeek); // ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+
