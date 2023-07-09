ccd943b350f5afc43ddc8edfed93aebb50cc8b00
niyamallo <treenicole@naver.com>
2023-07-09 23:33:55 +0900

    20230709

--- ChromeAPP/app.js
+++ ChromeAPP/app.js
@@ -1,9 +1,30 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText = "Hello " + username; paintGreetings에 사용한 방식이 더 좋음
+    paintGreetings(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;  // 백틱 사용
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername =localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    greeting.innerText = `Hello ${savedUsername}`;
+    paintGreetings(savedUsername)
+}
